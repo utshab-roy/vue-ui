f470822895d7896c2979b60e52078b7455392dc0
Utshab Roy <utshab.roy@gmail.com>
2022-02-28 22:32:32 +0900

    initial vue 3 app with vite

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div>{{ greet }} {{ name }}</div>
+<div v-text="color"></div>
+  
+</template>
+
+<script>
+export default {
+  name: "App", 
+  data() {
+    return {
+      name: "Roy", 
+      greet: "Hello",
+      color: "Green"
+    }
+  }
+}
+
+</script>
+
+<style>
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
