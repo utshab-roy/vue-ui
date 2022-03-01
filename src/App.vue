2d47236e7568521323daa4b68239bbd06111e1df
Utshab Roy <utshab.roy@gmail.com>
2022-03-01 19:05:37 +0900

    basic v-for binding demo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
 
-<div v-bind:id="color">{{ greet }} {{ name }}</div>
-<div v-text="color" v-bind:class="italicBool && 'underline'"></div>
-<div :class="italicBool ? 'underline' : 'text-bold'">Style Test</div>
+<!-- <h2 v-for="name, index in names" :key="name"> {{ index }} {{ name }}</h2> -->
+<h2 v-for="name in fullName" :key="name"> {{ name.firstName }} {{ name.lastName }}</h2>
+
+<div v-for="actor in actors" :key="actor.name">
+    <h3>{{ actor.name }}</h3>
+    <h4 v-for="movie in actor.movies" :key="movie">{{ movie }}</h4>
+</div>
   
 </template>
 
@@ -11,10 +15,23 @@ export default {
   name: "App", 
   data() {
     return {
-      name: "Roy", 
-      greet: "Hello",
-      color: "Green",
-      italicBool: false,
+      names: ['Diana', 'Bruch', 'Peter'],
+      fullName: [
+        {firstName: 'Utshab', lastName: 'Roy'},
+        {firstName: 'Ashik', lastName: 'Jhalok'}
+      ],
+      actors: [
+        {
+          name:'Bale',
+          movies: ['Batman', 'Yuma']
+        },
+        {
+          name:'Di cap',
+          movies: ['Titanic', 'Wolf', 'Inception']
+        }
+      ]
+      
+      
     }
   }
 }
