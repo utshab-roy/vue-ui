11d562f43027d8397fb7f0ca93893a8df5ef6840
Utshab Roy <utshab.roy@gmail.com>
2022-03-01 19:54:15 +0900

    basic demo of methods in vue

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,26 @@
 <template>
-
-<!-- <h2 v-for="name, index in names" :key="name"> {{ index }} {{ name }}</h2> -->
-<h2 v-for="name in fullName" :key="name"> {{ name.firstName }} {{ name.lastName }}</h2>
-
-<div v-for="actor in actors" :key="actor.name">
-    <h3>{{ actor.name }}</h3>
-    <h4 v-for="movie in actor.movies" :key="movie">{{ movie }}</h4>
-</div>
-  
+  <div>{{ add(5, 8) }}</div>
+  <h2>{{ multiply(baseValue) }}</h2>
 </template>
 
 <script>
 export default {
-  name: "App", 
+  name: "App",
   data() {
     return {
-      names: ['Diana', 'Bruch', 'Peter'],
-      fullName: [
-        {firstName: 'Utshab', lastName: 'Roy'},
-        {firstName: 'Ashik', lastName: 'Jhalok'}
-      ],
-      actors: [
-        {
-          name:'Bale',
-          movies: ['Batman', 'Yuma']
-        },
-        {
-          name:'Di cap',
-          movies: ['Titanic', 'Wolf', 'Inception']
-        }
-      ]
-      
-      
+      baseMultiplier: 5,
+      baseValue: 2
     }
-  }
-}
-
+  },
+  methods: {
+    add(a, b) {
+      return a + b
+    },
+    multiply(num) {
+      return num * this.baseMultiplier
+    }
+  },
+};
 </script>
 
 <style>
@@ -58,6 +43,4 @@ export default {
 .text-bold {
   font-weight: bold;
 }
-
-
 </style>
