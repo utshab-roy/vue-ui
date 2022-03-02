519dc608c8e261d0afec1b4e3ea31368ae89f23d
Utshab Roy <utshab.roy@gmail.com>
2022-03-02 10:56:49 +0900

    getter and setter created for computed proparty demo

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,15 @@
 <template>
+
   <div>
     <pre>
-      {{ JSON.stringify(formValues, null, 2) }}
+      {{ JSON.stringify(firstName, null, 2) }}
+      {{ JSON.stringify(lastName, null, 2) }}
     </pre>
+
+    <p>{{ fullName }}</p>
+
+    <button @click="changeName">Change Name</button>
   </div>
-
-<!-- using preventDefault as modifyer -->
-  <form @submit.prevent="submitForm">
-
-    <div>
-      <label for="name">Name:</label>
-      <input type="text" id="name" v-model.trim.lazy="formValues.name">
-    </div>
-
-     <div>
-      <label for="profile">Profile Summary:</label>
-      <textarea id="profile" v-model="formValues.profileSummary" />
-    </div>
-
-    <div>
-      <label for="country">Country:</label>
-      <select id="country" v-model="formValues.country">
-          <option value=""></option>
-          <option value="bangladesh">Bangladesh</option>
-          <option value="japan">Japan</option>
-          <option value="usa">USA</option>
-      </select>
-    </div>
-
-    <div>
-      <label for="job-location">Job Location:</label>
-      <select id="job-location" multiple v-model="formValues.jobLocation">
-          <option value=""></option>
-          <option value="bangladesh">Bangladesh</option>
-          <option value="japan">Japan</option>
-          <option value="usa">USA</option>
-      </select>
-    </div>
-  
-    <div>
-      <input type="checkbox" id="remote-work" 
-      v-model="formValues.remoteWork" 
-      true-value="yes" 
-      false-value="no" />
-      <label for="remoteW-work">Open for remote work ?</label>
-    </div>
-
-    <div>
-      <label>Skill Set</label>
-      <input type="checkbox" id="html" value="html" v-model="formValues.skillSet"/>
-      <label for="html">HTML</label>
-      <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet"/>
-      <label for="javascript">JavaScript</label>
-      <input type="checkbox" id="css" value="css" v-model="formValues.skillSet"/>
-      <label for="css">CSS</label>
-    </div>
-
-    <div>
-      <label>Years of Experience</label>
-      <input type="radio" id="0-2" value="0-2" v-model="formValues.yearOfExperience"/>
-      <label for="0-2">0-2</label>
-      <input type="radio" id="3-5" value="3-5" v-model="formValues.yearOfExperience"/>
-      <label for="3-5">3-5</label>
-      <input type="radio" id="5-10" value="5-10" v-model="formValues.yearOfExperience"/>
-      <label for="5-10">5-10</label>
-    </div>
-
-    <div>
-      <label for="age">Age</label>
-      <!-- using number modifyer to make the input store as number it will not act as string -->
-       <input type="number" id="age" v-model.number="formValues.age"/>
-    </div>
-
-    <div>
-      <button type="submit">Submit</button>
-    </div>
-
-  </form>
 
 </template>
 
@@ -85,25 +18,28 @@ export default {
   name: "App",
   data() {
     return {
-      formValues: {
-        name: "",
-        profileSummary: "",
-        country: "",
-        jobLocation: [],
-        // this can be boolean value, to use it no need of true-value, false value
-        remoteWork: 'no', 
-        skillSet: [],
-        yearsOfExperience: '',
-        age: null,
-
-      }
+      firstName: 'John',
+      lastName: 'Doe',
     };
   },
   methods: {
-    submitForm() {
-      console.log('Form values', this.formValues)
+    changeName() {
+      this.fullName = 'Utshab Roy';
+
     }
   },
+  computed: {
+    fullName: {
+      get() {
+        return `${this.firstName} ${this.lastName}`;
+      },
+      set(value) {
+        const names = value.split(' ')
+        this.firstName = names[0] 
+        this.lastName = names[1] 
+      }
+    }
+  }
 };
 </script>
 
