beb1d4704d2fca327891012b6829fb75b982fede
Utshab Roy <utshab.roy@gmail.com>
2022-03-03 18:21:08 +0900

    simple validation added in the emit custom event demo

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -2,14 +2,28 @@
     <div>
         <h2>This is a pop-up</h2>
 <!-- here the $emit first parameter is the event name and the second parameter is data -->
-        <button @click="$emit('close', 'Roy')">Close</button>
+        <input type="text" v-model="name"/>
+        <button @click="$emit('close', name)">Close</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Popup',
-        emits: ['close']
+        emits: {
+            close: (name) => {
+                if (!name) {
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        },
+        data() {
+            return {
+                name: ''
+            }
+        }
     }
 </script>
 
