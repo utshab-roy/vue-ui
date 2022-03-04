ae817a9d1cfbb589e42b841dce93c1d48e0dc8a6
Utshab Roy <utshab.roy@gmail.com>
2022-03-04 15:03:22 +0900

    basic demo of v-model bind on custom component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <template>
-    <button @click="showPopup = true">Show Popup</button>
-    <Popup v-show="showPopup" @close="closePopup" />
+    <Input v-model="name" />
 </template>
 
 <script>
-import Popup from "./components/Popup.vue";
-
+import Input from "./components/Input.vue";
 
 export default {
     name: "App",
     data() {
         return {
-            showPopup: false,
+            name: '',
         }
     },
     methods: {
-        closePopup(name) {
-            this.showPopup = false;
-            console.log(`The name: ${name} came from Popup component and render to App component `);
-        }
     },
 
     components: {
-    Popup
-},
-    provide() { // the provide method pass the username to the child component
-        return {
-            username: this.name
-        }
-    }
+    Input
+}
 }
 </script>
 
@@ -39,30 +28,5 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-}
-
-label {
-    font-weight: bold;
-    display: flex;
-    margin-bottom: 5px;
-}
-input + label {
-    font-weight: bold;
-    display: inline-flex;
-    margin-right: 20px;
-}
-input[type="text"],
-textarea,
-select {
-    display: block;
-    width: 400px;
-    padding: 6px 12px;
-    font-size: 14px;
-    line-height: 1.42857143;
-    color: #555;
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #ccc;
-    border-radius: 4px;
 }
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,43 @@
+<template>
+    <input 
+        type="text" 
+        :value="modelValue" 
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    props: {
+        modelValue: String
+    }
+}
+</script>
+
+<style scoped>
+label {
+    font-weight: bold;
+    display: flex;
+    margin-bottom: 5px;
+}
+input + label {
+    font-weight: bold;
+    display: inline-flex;
+    margin-right: 20px;
+}
+input[type="text"],
+textarea,
+select {
+    display: block;
+    width: 400px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: #555;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+</style>
