6d25a69b0e8831f64acd5df8f3595ce6be5b2ef1
Utshab Roy <utshab.roy@gmail.com>
2022-03-06 20:33:35 +0900

    diff of ref and reactive data, and use case, advantage

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -3,26 +3,53 @@
         <h2>Option API {{ o_firstName }}</h2>
         <h2>Composition API {{ c_firstName }}</h2>
         <h2>Composition from greet const: {{ greet }}</h2>
+        <h2>Composition from reactive const: {{ reactiveGreetHero }}</h2>
     </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 export default {
     name: 'Data',
     setup() {
-        const c_firstName = ref('Bipro')
-        console.log('c_firstName', c_firstName);
-        c_firstName.value = 'Diana'
-        const greet = `Hello, ${c_firstName.value}`
+        /**
+         * use ref() for primitive type, int, String, Bool
+         */
+        const c_firstName = ref('Bruce')
+        const c_lastName = ref('Wayne')
+        const c_heroName = ref('Batman')
+
+        // console.log('c_firstName', c_firstName);
+        // c_firstName.value = 'Princess'
+        // c_lastName.value = 'Diana'
+        // c_heroName.value = 'Wonder Woman'
+        const greet = `Hello, ${c_firstName.value} ${c_firstName.value} a.k.a ${c_heroName.value}`
+
+        /**
+         * reactive can only accept object
+         */
+        const  state = reactive({
+            firstName: 'Princess',
+            lastName : 'Diana',
+            heroName : 'Wonder Woman',
+        })
+
+        const reactiveGreetHero = `Hello from reactive setup ${state.firstName} ${state.firstName} a.k.a ${state.heroName}`
+
         return {
             c_firstName,
-            greet
+            c_lastName,
+            c_heroName,
+            greet,
+            state,
+            reactiveGreetHero,
         }
     },
     data() {
         return {
-            o_firstName: 'Utshab'
+            o_firstName: 'Clark',
+            o_lastName: 'Kent',
+            o_HeroName: 'Superman',
         }
     },
 }
