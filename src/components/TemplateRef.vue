c41fcd6ad42159707f521a245e4f692e839a0443
Utshab Roy <utshab.roy@gmail.com>
2022-03-06 13:22:59 +0900

    ref can be used in the child component demo

--- src/components/TemplateRef.vue
+++ src/components/TemplateRef.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
         <input type="text" ref="inputRef"/>
+        <PostList ref="postListRef"/>
     </div>
 </template>
 
 <script>
+import PostList from "./PostList.vue"
     export default {
-        name: 'TemplateRef',
-        mounted(){
-            this.$refs.inputRef.focus()
-        }
-    }
+    name: "TemplateRef",
+    mounted() {
+        this.$refs.inputRef.focus()
+        console.log(this.$refs.postListRef);
+    },
+    components: { PostList }
+}
 </script>
 
 <style scoped>
