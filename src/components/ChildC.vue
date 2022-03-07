b8ae1e82b4cb6ad171e0fd5256280c37f7e200ef
Utshab Roy <utshab.roy@gmail.com>
2022-03-07 12:26:23 +0900

    provide and inject demo with composition api

--- src/components/ChildC.vue
+++ src/components/ChildC.vue
@@ -2,13 +2,33 @@
     <div>
         <h2>ChildC Component</h2>
         <h3>Username is: {{ username }}</h3>
+        <h3>Username is: {{ childUserName }}</h3>
+
+        <h3>Child component C count - {{ childCount }}</h3>
+        <h3>Child component C Hero name - {{ firstName }} {{ lastName }}</h3>
+
+        <button @click="incrementCount">Increment form Child component</button>
     </div>
 </template>
 
 <script>
-
+import { inject, toRefs } from "vue";
 export default {
     name: 'ChildC',
+    setup() {
+        const childUserName = inject('c_userName', 'default USERNAME')
+        const childCount = inject('c_count', 0)
+        const childHero = inject('c_hero', {})
+        const incrementCount = inject('incrementCount')
+
+        return {
+            childUserName,
+            childCount,
+            ...toRefs(childHero),
+            incrementCount,
+        }
+    },
+
     inject: ['username']
 }
 </script>
--- src/components/ProvideInject.vue
+++ src/components/ProvideInject.vue
@@ -1,14 +1,44 @@
 <template>
     <div>
         <h2>Parent Component: {{ name }}</h2>
+        <h2>Parent Component count: {{ count }}</h2>
+        <h2>Parent Component hero: {{ firstName }} {{ lastName }}</h2>
+
+        <button @click="incrementCount">Increment Count</button>
+
         <ChildA />
     </div>
 </template>
 
 <script>
+import { provide, ref, reactive, toRefs } from "vue"
 import ChildA from './ChildA.vue'
 export default {
     name: 'ProvideInject',
+    setup() {
+        provide('c_userName', 'Roy (From composition api)')
+
+        const count = ref(0)
+
+        function incrementCount() {
+            count.value++
+        }
+
+        const state = reactive({
+            firstName: 'Bruce',
+            lastName: 'Wayne',
+        })
+
+        provide('c_count', count)
+        provide('c_hero', state)
+        provide('incrementCount', incrementCount)
+
+        return {
+            count,
+            ...toRefs(state),
+            incrementCount
+        }
+    },
     data() {
         return {
             name: 'Batman'
