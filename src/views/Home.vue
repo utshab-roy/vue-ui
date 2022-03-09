c59dcea553f5d5a0189462709be50b7c9a10d0d5
Utshab Roy <utshab.roy@gmail.com>
2022-03-09 15:27:10 +0900

    named route for destination impl, for dynamic routing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,8 @@
 		<h1>All Destinations</h1>
 
 		<div class="destinations">
-			<routerLink v-for="destination in destinations" :key="destination.id" :to="destination.slug">
+			<routerLink v-for="destination in destinations" :key="destination.id" 
+			:to="{name: 'destination.show', params: {id: destination.id}}">
 				<h2>{{ destination.name }}</h2>
 				<img :src="`/images/${destination.image}`" :alt="destination.name" />
 			</routerLink>
