3308e54d0c8882211bf2a175a1470fa2c00a4b76
Utshab Roy <utshab.roy@gmail.com>
2022-03-11 18:52:09 +0900

    NotFound page include with basic validation and keep the wrong link in the URL

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import sourceData from '@/Data.json'
 
 
 const router = createRouter({
@@ -13,6 +14,20 @@ const router = createRouter({
 			component: () => import('@/views/DestinationShow.vue'),
 			// we used ...route.params so that we can get all the route parameter value
 			props: route => ({ ...route.params, id: parseInt(route.params.id) }),
+			beforeEnter(to, from) {
+				const exists = sourceData.destinations.find(
+					destination => destination.id === parseInt(to.params.id)
+				)
+
+				if (!exists) return {
+					name: 'NotFound',
+					// allows keeping the URL while rendering a different page
+					params: { pathMatch: to.path.split('/').slice(1) },
+					query: to.query,
+					hash: to.hash,
+				}
+
+			},
 			// nested route example
 			children: [
 				{
@@ -24,13 +39,11 @@ const router = createRouter({
 				},
 			]
 		},
-		// {
-		// 	path: '/destination/:id/:slug/:experienceSlug',
-		// 	name: 'experience.show',
-		// 	component: () => import('@/views/ExperienceShow.vue'),
-		// 	// we used ...route.params so that we can get all the route parameter value, id, slug, experienceSlug
-		// 	props: route => ({ ...route.params, id: parseInt(route.params.id) }),
-		// },
+		{
+			path: '/:pathMatch(.*)*',
+			name: 'NotFound',
+			component: () => import('@/views/NotFound.vue'),
+		},
 	],
 	// linkActiveClass: 'router-link-active', // this class will be added automatically to the active link
 })
