d0b0830657da2420f7586867103d238d140e2ff9
Utshab Roy <utshab.roy@gmail.com>
2022-03-11 18:09:31 +0900

    simple back button impl in vue

---
+++ src/components/GoBack.vue
@@ -0,0 +1,11 @@
+<template>
+    <span class="go-back">
+        <button @click="$router.back()">Go Back</button>
+    </span>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="destination">
         <h1>{{ destination.name }}</h1>
+        <GoBack />
         <div class="destination-details">
             <img :src="`/images/${destination.image}`" :alt="destination.name" />
             <p>{{ destination.description }}</p>
@@ -24,11 +25,13 @@
 <script>
 import sourceData from '@/data.json'
 import ExperienceCard from "@/components/ExperienceCard.vue";
+import GoBack from '../components/GoBack.vue';
 
 export default {
     components: {
-        ExperienceCard,
-    },
+    ExperienceCard,
+    GoBack
+},
     props: {
         id: {
             type: Number,
