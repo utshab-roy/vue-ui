78a1b9dc4fa9c8a14465ba9611fff556debb28cb
Utshab Roy <utshab.roy@gmail.com>
2022-03-14 15:47:01 +0900

    routes array seperate and added in a const

--- src/router/index.js
+++ src/router/index.js
@@ -2,49 +2,50 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import sourceData from '@/Data.json'
 
+const routes = [
+	{ path: '/', name: 'home', component: Home },
+	// which is lazy-loaded when the route is visited
+	{
+		path: '/destination/:id/:slug',
+		name: 'destination.show',
+		component: () => import('@/views/DestinationShow.vue'),
+		// we used ...route.params so that we can get all the route parameter value
+		props: route => ({ ...route.params, id: parseInt(route.params.id) }),
+		beforeEnter(to, from) {
+			const exists = sourceData.destinations.find(
+				destination => destination.id === parseInt(to.params.id)
+			)
+
+			if (!exists) return {
+				name: 'NotFound',
+				// allows keeping the URL while rendering a different page
+				params: { pathMatch: to.path.split('/').slice(1) },
+				query: to.query,
+				hash: to.hash,
+			}
+
+		},
+		// nested route example
+		children: [
+			{
+				path: ':experienceSlug',
+				name: 'experience.show',
+				component: () => import('@/views/ExperienceShow.vue'),
+				// we used ...route.params so that we can get all the route parameter value, id, slug, experienceSlug
+				props: route => ({ ...route.params, id: parseInt(route.params.id) }),
+			},
+		]
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'NotFound',
+		component: () => import('@/views/NotFound.vue'),
+	},
+]
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{ path: '/', name: 'home', component: Home },
-		// which is lazy-loaded when the route is visited
-		{
-			path: '/destination/:id/:slug',
-			name: 'destination.show',
-			component: () => import('@/views/DestinationShow.vue'),
-			// we used ...route.params so that we can get all the route parameter value
-			props: route => ({ ...route.params, id: parseInt(route.params.id) }),
-			beforeEnter(to, from) {
-				const exists = sourceData.destinations.find(
-					destination => destination.id === parseInt(to.params.id)
-				)
-
-				if (!exists) return {
-					name: 'NotFound',
-					// allows keeping the URL while rendering a different page
-					params: { pathMatch: to.path.split('/').slice(1) },
-					query: to.query,
-					hash: to.hash,
-				}
-
-			},
-			// nested route example
-			children: [
-				{
-					path: ':experienceSlug',
-					name: 'experience.show',
-					component: () => import('@/views/ExperienceShow.vue'),
-					// we used ...route.params so that we can get all the route parameter value, id, slug, experienceSlug
-					props: route => ({ ...route.params, id: parseInt(route.params.id) }),
-				},
-			]
-		},
-		{
-			path: '/:pathMatch(.*)*',
-			name: 'NotFound',
-			component: () => import('@/views/NotFound.vue'),
-		},
-	],
+	routes,
 	// linkActiveClass: 'router-link-active', // this class will be added automatically to the active link
 })
 
